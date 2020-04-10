e0f8d838c6cc78a469a4733591b3d823fdc7ab9d
noomdalv <vladlunasan@gmail.com>
2020-04-10 18:24:12 -0500

    implemented session_store initializer to work with dev environment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
-Rails.application.config.session_store :cookie_store,
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store,
   key: "_authentication_app",
   domain: "http://trackerapi-vls.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store,
+  key: "_authentication_app"  
+end
