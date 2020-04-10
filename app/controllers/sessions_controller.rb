10555d18f53f8210ecfbfb6a1af4c487940875cf
noomdalv <vladlunasan@gmail.com>
2020-04-10 17:49:50 -0500

    added registrations controller and respective route for user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def create
     user = User
             .find_by(email: params["user"]["email"])
-            .try(:authenticate), params["user"]["password"])
+            .try(:authenticate, params["user"]["password"])
     if user
-      session[:user_if] = user.id
+      session[:user_id] = user.id
       render json: {
         status: :created,
         logged_in: true,
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       }
     else
       render json: { status: 401 }
-    end    
+    end
   end
 
 
