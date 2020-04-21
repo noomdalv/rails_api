ce969c289e5580e755548dcb23574b9d34912e9b
noomdalv <vladlunasan@gmail.com>
2020-04-20 21:26:33 -0500

    added live link for cors initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,14 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://reactactivitytracker.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
                   credentials: true
   end
 
   allow do
-    origins "https://reactactivitytracker.herokuapp.com"
+    origins "http://localhost:3000"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
                   credentials: true
   end
+
 end
