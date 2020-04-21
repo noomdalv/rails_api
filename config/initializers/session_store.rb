9fdd137f08ca94929bf8cd4f363bcba52a3cb7e0
noomdalv <vladlunasan@gmail.com>
2020-04-21 10:29:19 -0500

    set httponly false for cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,9 @@
 if Rails.env == "production"
   Rails.application.config.session_store :cookie_store,
   key: "_authentication_app",
-  domain: "https://activitytrackerapi.herokuapp.com"
+  domain: "https://activitytrackerapi.herokuapp.com",
+  httponly: false
+
 else
   Rails.application.config.session_store :cookie_store,
   key: "_authentication_app"
