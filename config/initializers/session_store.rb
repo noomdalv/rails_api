1e27e3f4ec73e72c1119ea97deaadb4ec416f60c
noomdalv <vladlunasan@gmail.com>
2020-04-20 23:40:00 -0500

    cookie_store debug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
-if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store,
+Rails.application.config.session_store :cookie_store,
   key: "_authentication_app",
   domain: "https://activitytrackerapi.herokuapp.com/"
-else
-  Rails.application.config.session_store :cookie_store,
-  key: "_authentication_app"
-end
