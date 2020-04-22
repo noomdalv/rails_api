a956322773d613558c88cbae0f31845d0265d647
noomdalv <vladlunasan@gmail.com>
2020-04-22 00:34:18 -0500

    fixed else typo in session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,7 @@ if Rails.env == "production"
   key: "_authentication_app",
   domain: "https://activitytrackerapi.herokuapp.com",
   secure: true,
-  same_site: lax,
+  same_site: lax
 else
   Rails.application.config.session_store :cookie_store,
   key: "_authentication_app"
