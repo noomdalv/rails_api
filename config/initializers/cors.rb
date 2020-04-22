976e5c1fdf010828010edfacca1133114ca0a56e
noomdalv <vladlunasan@gmail.com>
2020-04-22 00:30:00 -0500

    added secure and same_site attribs for cookies

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,18 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allowed_methods = %i[ get post put patch delete options head ]
+Rails.application.config.middleware.insert_before 0, Rack::Cors do  
 
   allow do
     origins "https://reactactivitytracker.herokuapp.com"
-    resource "*", headers: :any, methods: allowed_methods,
+    resource "*", headers: :any,
+    methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true,
+    secure: true,
+    same_site: lax
   end
 
   allow do
     origins "localhost:3000"
-    resource "*", headers: :any, methods: allowed_methods,
+    resource "*", headers: :any,
+    methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
 end
