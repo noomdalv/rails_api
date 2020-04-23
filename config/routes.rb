7b464455e103ee5022d4538f55b2c67bf2df3690
noomdalv <vladlunasan@gmail.com>
2020-04-22 22:22:01 -0500

    fixed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'static#home'
+
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
   resources :records, only: [:create, :index]
   delete :logout, to: "sessions#logout"
   post :logged_in, to: "sessions#logged_in"
-  root 'welcome#index'
+
 end
